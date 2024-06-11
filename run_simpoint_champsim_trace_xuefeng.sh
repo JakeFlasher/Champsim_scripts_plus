b884a1e6a07fe4d63a656f8e35e070fc4db2b0a4
JakeFlasher <31953787+JakeFlasher@users.noreply.github.com>
2024-06-12 02:40:11 +0800

    Update run_simpoint_champsim_trace_xuefeng.sh

--- run_simpoint_champsim_trace_xuefeng.sh
+++ run_simpoint_champsim_trace_xuefeng.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+# sizeof returned: 64 for trace inst type
+# raw
+../qemu/build/qemu-x86_64 -D log.txt -d plugin -plugin build/libbbv_ibar.so,name=/mnt/d/Stockfish_bbv,size=100000000 -- /usr/games/stockfish bench 128 1 24 default depth
 
+../Simpoint.3.2_fix/bin/simpoint -maxK 10 -loadFVFile /mnt/d/Stockfish_bbv/bbv -saveSimpoints /mnt/d/Stockfish_bbv/simpoints -saveSimpointWeights /mnt/d/Stockfish_bbv/weights
+
+env TRACE_FILENAME=/mnt/d/Stockfish_bbv/champsim.trace SIMPOINT_FILE= /mnt/d/Stockfish_bbv/simpoints BB_INTERVAL=100000000  ../qemu/build/qemu-x86_64 -D log.txt -d plugin -plugin build/libchampsim.so -- /usr/games/stockfish bench 128 1 24 default depth
 set -e
 
 output=$1
